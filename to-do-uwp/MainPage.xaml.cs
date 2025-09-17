2239ea7d998840bc19c7c740005255e9cc90f106
Jacob Kirchner <jacobkir500@gmail.com>
2025-09-17 11:53:30 -0500

    added summaries of pages

--- to-do-uwp/MainPage.xaml.cs
+++ to-do-uwp/MainPage.xaml.cs
@@ -13,12 +13,10 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace to_do_uwp
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// The Main Page of the application
     /// </summary>
     public sealed partial class MainPage : Page
     {
--- to-do-uwp/Settings.xaml.cs
+++ to-do-uwp/Settings.xaml.cs
@@ -13,12 +13,10 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace to_do_uwp
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// A Settings Page
     /// </summary>
     public sealed partial class Settings : Page
     {
