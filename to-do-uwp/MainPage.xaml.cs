37358e8f07c4f7c4315b605a2fcf35811b8ce657
Jacob Kirchner <jacobkir500@gmail.com>
2025-09-17 12:42:13 -0500

    added task class and data serialization

--- to-do-uwp/MainPage.xaml.cs
+++ to-do-uwp/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,9 +25,55 @@ namespace to_do_uwp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private static ObservableCollection<Models.task> tasks; 
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            StorageFolder current = ApplicationData.Current.LocalFolder;
+            try
+            {
+                StorageFile file = await current.GetFileAsync("tasks.json");
+                string json = await FileIO.ReadTextAsync(file);
+                tasks = JsonConvert.DeserializeObject<ObservableCollection<Models.task>>(json);
+                if (tasks == null)
+                {
+                    tasks = new ObservableCollection<Models.task>();
+                }
+                //TasksListView.ItemsSource = tasks;
+            }
+            catch (FileNotFoundException)
+            {
+                tasks = new ObservableCollection<Models.task>();
+                //TasksListView.ItemsSource = tasks;
+            }
+        }
+
+        protected override async void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            StorageFolder current = ApplicationData.Current.LocalFolder;
+            StorageFile file = await current.CreateFileAsync("tasks.json", CreationCollisionOption.ReplaceExisting);
+            await SaveToFile(file, JsonConvert.SerializeObject(tasks));
+        }
+
+        private async Task SaveToFile(StorageFile file, string content)
+        {
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                using (DataWriter dataWriter = new DataWriter(stream))
+                {
+                    dataWriter.WriteString(content);
+                    await dataWriter.StoreAsync();
+                }
+            }
         }
 
         private void CompletedTasksButton_Click(object sender, RoutedEventArgs e)
@@ -38,6 +89,11 @@ namespace to_do_uwp
         private void AboutButton_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(About));
+        }
+
+        private void AddTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
